824f6f09f3d1e2211ecd6c98039aa9f07d48cebb
Hannahcp822 <hannah123p@gmail.com>
2015-09-08 09:23:36 -0700

    09/08/15 - Fahrenheit to Celsius Conversion

---
+++ Fahrenheit to Celcius/FahrenheitToCelcius.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class FahrenheitToCelcius here.
+ * 
+ * @author Hannah Pang 
+ * @version September 8, 2015
+ */
+public class FahrenheitToCelcius
+{
+    public static void main(String[] args) 
+    {
+        double degreesCelcius = 20.0;
+        double degreesFahrenheit = (9/5) * (degreesCelcius + 32);
+        System.out.println(+degreesFahrenheit);
+    }
+}
+        
+        
