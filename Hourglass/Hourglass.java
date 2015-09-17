4695e8170be09c857534aa50fbf7a7b7f960242e
Hannahcp822 <hannah123p@gmail.com>
2015-09-17 08:52:31 -0700

    09/1715 - Hourglass Program
    Hannah Pang
    Printing an hourglass figure using nested for loops

---
+++ Hourglass/Hourglass.java
@@ -0,0 +1,35 @@
+
+/**
+ * This program uses nested for loops to print an hourglass figure.
+ * 
+ * @author Hannah Pang
+ * @version September 18, 2015
+ */
+public class Hourglass
+{
+   public static void main(String[] args)
+   {
+       printQuotes();
+       for (int i = 1; i <= 4; i++) 
+       {
+           for (int j = 1; j <= i; j++)
+           {
+               System.out.print(" "); 
+           }
+           System.out.print("\\");
+           for (int j = 8; j <= )
+           {
+               System.out.print(":");
+            }
+           System.out.println();
+       }
+       printPipelines();
+       for (int i = 1; i <= 4, i++)
+       {
+           for (int j = 1; j = 4 - i; j++)
+           {
+               System.out.print(" ");
+           }
+           System.out.print("/");
+           for (int j = 1; j <=  
+            
