5713346a317d7eba4292724bc0d6867036a2f39b
Hannahcp822 <hannahpang@Hannahs-Laptop.local>
2015-09-19 11:10:09 -0700

    09/19/15 - Hourglass Program
    Hannah Pang
    Programming Project 2.4
    Hourglass Program

--- Hourglass/Hourglass.java
+++ Hourglass/Hourglass.java
@@ -1,6 +1,7 @@
 
 /**
  * This program uses nested for loops to print an hourglass figure.
+ * (Programming Project 2.4)
  * 
  * @author Hannah Pang
  * @version September 18, 2015
@@ -10,26 +11,55 @@ public class Hourglass
    public static void main(String[] args)
    {
        printQuotes();
+       printTopHalf();
+       printPipelines();
+       printBottomHalf();
+       printQuotes();
+   }
+   
+   public static void printQuotes() //prints the first and last lines of the hourglass
+   {
+       System.out.println("|\"\"\"\"\"\"\"\"\"\"|");
+   }
+   
+   public static void printPipelines() //prints the two pipelines in the middle of the hourglass
+   {
+       System.out.println("     ||");
+   }
+   
+   public static void printTopHalf() //prints the top inverted triangle of the hourglass
+   {
        for (int i = 1; i <= 4; i++) 
        {
-           for (int j = 1; j <= i; j++)
+           for (int j = 1; j <= i; j++) //number of spaces increases as i increases
            {
                System.out.print(" "); 
            }
            System.out.print("\\");
-           for (int j = 8; j <= )
+           for (int k = 4; k >= i; k--) //number of "::" decreases as i increases
            {
-               System.out.print(":");
+               System.out.print("::");
             }
-           System.out.println();
+           System.out.println("/");
        }
-       printPipelines();
-       for (int i = 1; i <= 4, i++)
+   }
+   
+   public static void printBottomHalf() //prints the bottom triangle of the hourglass
+   {
+       for (int i = 1; i <= 4; i++)
        {
-           for (int j = 1; j = 4 - i; j++)
+           for (int j = 4; j >= i; j--) //number of spaces decreases as i increases
            {
                System.out.print(" ");
            }
            System.out.print("/");
-           for (int j = 1; j <=  
+           for (int k = 1; k <= i; k++) //number of "::" increases as i increases
+           {
+               System.out.print("::");
+           }
+           System.out.println("\\");
+       }
+   }
+}   
+           
             
