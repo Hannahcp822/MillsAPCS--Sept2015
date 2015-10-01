87c80a8b61923b1b90a3d5e60d47a7f7a3781d3a
Hannahcp822 <hannahpang@Hannahs-Laptop.local>
2015-09-30 22:02:05 -0700

    09/30/15 ASCII Project
    Sailboat:)

---
+++ ASCII Project/Sailboat.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Sailboat here.
+ * 
+ * @author Hannah Pang
+ * @version (a version number or a date)
+ */
+public class Sailboat
+{
+   public static void main (String[] args)
+   {
+       int height = 
+       int width = 
+       for (i = 1; i <= width; i++)
+       {
+           for (j = 1; j <= height; j++)
+           {
+               printSailboat();
+           }
+       }
+   }
+   
+   public static void printSailboat()
+   {
+       printSail(11,11);
+       printBoat();
+   }
+   
+   public static void printSail(int sailHeight,int sailWidth)
+   {
+       for (i = 1; i <= 
