e3724f491fb58ee3d4565bd84fa86bf116c036a9
Hannahcp822 <hannah123p@gmail.com>
2015-10-02 08:54:41 -0700

    10/02/15 - ASCII Project
    Penguin holding balloon

--- ASCII Project/Penguin.java
+++ ASCII Project/Penguin.java
@@ -1,52 +1,155 @@
 
 /**
- * ASCII Project - prints a penguin
+ * ASCII Project - prints a penguin holding a balloon
  * 
  * @author Hannah Pang
- * @version 10/02/15
+ * @version October 02, 2015
  */
 public class Penguin
 {
     public static void main(String[] args)
     {
-        int height = 1;
-        int width = 1;
-        for (int i = 1; i <= width; i++)
-        {
-            for (int j = 1; j <= height; j++)
-            {
-                printPenguin();
-            }
-        }
-    }
+                printPenguin(2,5);
+    }   
     
-    public static void printPenguin()
+    public static void printPenguin(int height, int width)
     {                           
-        System.out.println("                                   88888888");
-        System.out.println("                                  8888888888");
-        System.out.println("            (MMMMMMMMMMMMMM)      8888888888");
-        System.out.println("          (MMMMMMMMMMMMMMMMMM)    8888888888");
-        System.out.println("         (@        MM        @)    88888888");
-        System.out.println("        (@@     /\\    /\\     @@)      $");
-        System.out.println("        (&/     \\/    \\/     \\D)      |");
-        System.out.println("        (@       ______       @)      /");
-        System.out.println("         (@      \\\\  //      @)       |");
-        System.out.println("          @a      \\\\//      o@        \\");
-        System.out.println("        &######################D      |");
-        System.out.println("      (&&&####################DDD)    /");
-        System.out.println("     (&&&&(         ####      )DDD)   |");
-        System.out.println("    (&&&&&          ####       DDDDD) \\");
-        System.out.println("   (&&&&&(          ####       )DDDDD)|");
-        System.out.println("   &&&&&&&          |###       DDDDDDD/");
-        System.out.println("  (&&&&&&(          \\###       )DDDDDD)");
-        System.out.println("  &&&&&&&&           ####      DDDDDDDD");
-        System.out.println("  (&&&&&&&(                   )DDDDDDD)");
-        System.out.println("   &WWV &&&                   DDD VWWD");
-        System.out.println("    (WV  &&(                 )DD  VW)");
-        System.out.println("          &&                 DD");
-        System.out.println("           @@@@@@@@@@@@@@@@@@@");
-        System.out.println("            W@@@@@@@@@@@@@@@W");
-        System.out.println("            W      W W      W");
-        System.out.println("            WWW/\\WWW WWW/\\WWW");
-    }
-}        
+        for (int i = 1; i <= height; i++)
+        {
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("                                       88888888 ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("                                      8888888888");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("                (MMMMMMMMMMMMMM)      8888888888");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("              (MMMMMMMMMMMMMMMMMM)    8888888888");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("             (@        MM        @)    88888888 ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("            (@@     /\\    /\\     @@)      $     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("            (&/     \\/    \\/     \\D)      |     ");
+            }
+            System.out.println();   
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("            (@       ______       @)      /     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("             (@      \\\\  //      @)       |     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("              @a      \\\\//      o@        \\     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("            &######################D      |     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("          (&&&####################DDD)    /     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("         (&&&&(         ####      )DDD)   |     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("        (&&&&&          ####       DDDDD) \\     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("       (&&&&&(          ####       )DDDDD)|     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("       &&&&&&&          |###       DDDDDDD/     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("      (&&&&&&(          \\###       )DDDDDD)     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("      &&&&&&&&           ####      DDDDDDDD     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("      (&&&&&&&(                   )DDDDDDD)     ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("       &WWV &&&                   DDD VWWD      ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("        (WV  &&(                 )DD  VW)       ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("              &&                 DD             ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("               @@@@@@@@@@@@@@@@@@@              ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("                W@@@@@@@@@@@@@@@W              ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("                W      W W      W              ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.print("                WWW/\\WWW WWW/\\WWW              ");
+            } 
+            System.out.println();
+            for (int j = 1; j <= width; j++)
+            {
+                System.out.println();
+            } 
+        } 
+    } 
+}
